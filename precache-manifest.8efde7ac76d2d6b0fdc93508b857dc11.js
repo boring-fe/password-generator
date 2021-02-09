af8b93899c69c1dc7976128ff179843ab48d4d76
Illia Lutvynov <illial@wix.com>
2021-02-09 10:17:17 +0200

    Updates

---
+++ precache-manifest.8efde7ac76d2d6b0fdc93508b857dc11.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "100ee9f9cf61d42f77ac2cbb8001d954",
+    "url": "/boring-fe/password-generator/index.html"
+  },
+  {
+    "revision": "a3debc263e599916b7eb",
+    "url": "/boring-fe/password-generator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1eb8b2927f643d3816ee",
+    "url": "/boring-fe/password-generator/static/js/2.0f110493.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/boring-fe/password-generator/static/js/2.0f110493.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3debc263e599916b7eb",
+    "url": "/boring-fe/password-generator/static/js/main.6cf2ec15.chunk.js"
+  },
+  {
+    "revision": "dc2ce47ce8989f141585",
+    "url": "/boring-fe/password-generator/static/js/runtime-main.eea0d589.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/boring-fe/password-generator/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/boring-fe/password-generator/precache-manifest.8efde7ac76d2d6b0fdc93508b857dc11.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/boring-fe/password-generator/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
